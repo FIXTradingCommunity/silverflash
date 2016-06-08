413580ea2cf4f20456df2b199cc816b5c607c364
Don Mendelson <donmendelson@gmail.com>
2016-06-08 11:10:29 -0500

    Update session messages to conform to FIXP RC3 #15

--- silverflash-core/src/main/java/org/fixtrading/silverflash/fixp/messages/MessageSessionIdentifier.java
+++ silverflash-core/src/main/java/org/fixtrading/silverflash/fixp/messages/MessageSessionIdentifier.java
@@ -1,49 +1,38 @@
 /**
- *    Copyright 2015 FIX Protocol Ltd
+ * Copyright 2015 FIX Protocol Ltd
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
  */
 
 package org.fixtrading.silverflash.fixp.messages;
 
 import java.nio.ByteBuffer;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 
 import org.fixtrading.silverflash.fixp.SessionId;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.ContextDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.Decoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.EstablishDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.EstablishmentAckDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.FinishedReceivingDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.FinishedSendingDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.NegotiateDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.NegotiationRejectDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.NegotiationResponseDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.RetransmissionDecoder;
-import org.fixtrading.silverflash.fixp.messages.MessageDecoder.TerminateDecoder;
 
 /**
  * Retrieves session ID from a FIXP session message
+ * 
+ * Optimized to take advantage of change in FIXP RC3 to always put session ID in
+ * same position in session messages.
  * 
  * @author Don Mendelson
  *
  */
 public class MessageSessionIdentifier implements Function<ByteBuffer, UUID> {
 
-  private final MessageDecoder decoder = new MessageDecoder();
+  private final SbeMessageHeaderDecoder headerDecoder = new SbeMessageHeaderDecoder();
   private final byte[] sessionId = new byte[16];
 
   /*
@@ -55,59 +44,57 @@ public class MessageSessionIdentifier implements Function<ByteBuffer, UUID> {
     UUID uuid = null;
     int pos = buffer.position();
     try {
-      Optional<Decoder> optDecoder = decoder.wrap(buffer, pos);
-      if (optDecoder.isPresent()) {
-        final Decoder decoder = optDecoder.get();
-        switch (decoder.getMessageType()) {
-          case APPLIED:
-          case NOT_APPLIED:
-          case SEQUENCE:
-          case UNSEQUENCED_HEARTBEAT:
-            // Returns null.
-            break;
-          case NEGOTIATE:
-            ((NegotiateDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case NEGOTIATION_RESPONSE:
-            ((NegotiationResponseDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case NEGOTIATION_REJECT:
-            ((NegotiationRejectDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case ESTABLISH:
-            ((EstablishDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case ESTABLISHMENT_ACK:
-            ((EstablishmentAckDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case ESTABLISHMENT_REJECT:
-            break;
-          case TERMINATE:
-            ((TerminateDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case RETRANSMIT_REQUEST:
-            break;
-          case RETRANSMISSION:
-            ((RetransmissionDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case FINISHED_SENDING:
-            ((FinishedSendingDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case FINISHED_RECEIVING:
-            ((FinishedReceivingDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          case CONTEXT:
-            ((ContextDecoder) decoder).getSessionId(sessionId, 0);
-            break;
-          default:
-            return null;
-        }
-        uuid =  SessionId.UUIDFromBytes(sessionId);
+      final MessageType msgType = getMessageType(buffer, pos);
+      if (msgType == null) {
+        // Must be an application message
+        return null;
       }
+      
+      switch (msgType) {
+        case APPLIED:
+        case NOT_APPLIED:
+        case SEQUENCE:
+        case UNSEQUENCED_HEARTBEAT:
+          // Returns null.
+          break;
+        case NEGOTIATE:
+        case NEGOTIATION_RESPONSE:
+        case NEGOTIATION_REJECT:
+        case ESTABLISH:
+        case ESTABLISHMENT_ACK:
+        case ESTABLISHMENT_REJECT:
+        case TERMINATE:
+        case RETRANSMIT_REQUEST:
+        case RETRANSMISSION:
+        case FINISHED_SENDING:
+        case FINISHED_RECEIVING:
+        case CONTEXT:
+          getSessionId(buffer, pos+SbeMessageHeaderDecoder.getLength(), sessionId, 0);
+          uuid = SessionId.UUIDFromBytes(sessionId);
+          break;
+        default:
+          return null;
+      }
+
       return uuid;
     } finally {
       // Idempotent for buffer position
       buffer.position(pos);
     }
+  }
+
+  private MessageType getMessageType(ByteBuffer buffer, int offset) {
+    int schema = headerDecoder.wrap(buffer, offset).getSchemaId();
+    if (schema != SessionMessageSchema.SCHEMA_ID) {
+      return null;
+    }
+    int code = headerDecoder.getTemplateId();
+
+    return MessageType.getMsgType(code);
+  }
+
+  private void getSessionId(ByteBuffer buffer, int offset, byte[] dest, int destOffset) {
+    buffer.position(offset);
+    buffer.get(dest, destOffset, 16);
   }
 }
